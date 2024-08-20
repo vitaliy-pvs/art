1212df76fabc10e41c90d846440c9b1a031d2659
vitaliy-pvs <vitaliypryhodko@yandex.com>
2024-08-20 21:01:04 +0300

    Adding test_h2

--- tests/ui_test.py
+++ tests/ui_test.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.common.by import By
 
 opts = FirefoxOptions()
 opts.add_argument("--headless")
@@ -11,10 +12,16 @@ class UiTestMhk01(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox(options=opts)
 
-    def test_page(self):
+    def test_title(self):
         driver = self.driver
         driver.get("http://195.58.54.98")
-        assert "МШК-01" in driver.title
+        # assert "МШК-01" in driver.title
+        assert driver.title == "МШК-01"
+
+    def test_h2(self):
+        driver = self.driver
+        driver.get("http://195.58.54.98")
+        assert driver.find_element(By.TAG_NAME, "h2").text == "Конфигуратор МШК-01"
 
     def tearDown(self):
         self.driver.close()
