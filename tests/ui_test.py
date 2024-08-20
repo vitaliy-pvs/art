e8d59da39b3786edd3ae8bcb91ef4e027ec20cda
unknown <vitaliypryhodko@yandex.com>
2024-08-20 16:35:39 +0300

    unittest add testCase

--- tests/ui_test.py
+++ tests/ui_test.py
@@ -1,15 +1,24 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 
 opts = FirefoxOptions()
 opts.add_argument("--headless")
 
-driver = webdriver.Firefox(options=opts)
-driver.get("http://195.58.54.98")
-assert "МШК-01" in driver.title
-driver.get("http://195.58.54.98/art04/")
-assert "МШК-04" in driver.title
-driver.get("http://195.58.54.98/art06/")
-assert "МШК-06" in driver.title
-driver.quit()
-print("Test successful!")
+
+class UiTestMhk01(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox(options=opts)
+
+    def test_page(self):
+        driver = self.driver
+        driver.get("http://195.58.54.98")
+        assert "МШК-01" in driver.title
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
