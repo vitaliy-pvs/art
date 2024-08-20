f765c1b6a7eb2b28b3950f9b8185fee5507a66db
unknown <vitaliypryhodko@yandex.com>
2024-08-20 11:01:27 +0300

    build>deploy>test

--- tests/ui_test.py
+++ tests/ui_test.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-driver.get("http://localhost:8000")
+driver.get("http://195.58.54.98")
 assert "МШК-01" in driver.title
+driver.get("http://195.58.54.98/art04/")
+assert "МШК-04" in driver.title
+driver.get("http://195.58.54.98/art06/")
+assert "МШК-06" in driver.title
 driver.quit()
+print("Test successful!")
