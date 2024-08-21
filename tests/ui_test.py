0b1e1144a1668ea329b2bc06e6a324bfd34dfd0c
unknown <vitaliypryhodko@yandex.com>
2024-08-21 14:23:10 +0300

    Test warningContainer

--- tests/ui_test.py
+++ tests/ui_test.py
@@ -1,7 +1,9 @@
+import time
 import unittest
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
+from selenium.webdriver import FirefoxOptions, ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 opts = FirefoxOptions()
 opts.add_argument("--headless")
@@ -15,13 +17,30 @@ class UiTestMhk01(unittest.TestCase):
     def test_title(self):
         driver = self.driver
         driver.get("http://195.58.54.98")
-        # assert "МШК-01" in driver.title
         assert driver.title == "МШК-01"
 
     def test_h2(self):
         driver = self.driver
         driver.get("http://195.58.54.98")
         assert driver.find_element(By.TAG_NAME, "h2").text == "Конфигуратор МШК-01"
+
+    def test_warningContainer(self):
+        driver = self.driver
+        driver.get("http://195.58.54.98")
+
+        select_td_el = driver.find_element(By.ID, "td")
+
+        # select_td = Select(select_td_el)
+        # select_td.select_by_visible_text("НЕТ")
+        select_td_el.click()
+        select_opt_os = select_td_el.find_element(By.XPATH, "//option[@value='2']")
+        select_opt_os.click()
+
+        warning_container = driver.find_element(By.ID, "warning_container")
+        print(warning_container.find_element(By.TAG_NAME, "p").text)
+        time.sleep(5)
+        print(warning_container.find_element(By.TAG_NAME, "p").text)
+        assert warning_container.find_element(By.TAG_NAME, "p").text == "- Отдельно стоящий механизм (ОС) должен иметь диван."
 
     def tearDown(self):
         self.driver.close()
