83299d7af8aab5cbe22b2e6ba989e5d27e2ccccd
unknown <vitaliypryhodko@yandex.com>
2024-08-22 16:45:11 +0300

    Refactor test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -20,10 +20,25 @@ class UiTestMhk01(unittest.TestCase):
     def test_h2(self):
         assert self.driver.find_element(By.TAG_NAME, "h2").text == "Конфигуратор МШК-01"
 
-    def test_warningContainer(self):
+    def test_condition_1(self):
+        select_os_el = self.driver.find_element(By.ID, "os")
+        select_os = Select(select_os_el)
+        select_os.select_by_visible_text("НЕТ")
+
+        select_td_el = self.driver.find_element(By.ID, "td")
+        select_td = Select(select_td_el)
+        selected_options_td = select_td.all_selected_options
+
+        for option in selected_options_td:
+            print(option.text)
+
+        warning_container = self.driver.find_element(By.ID, "warning_container")
+        assert warning_container.find_element(By.TAG_NAME, "p").text == "Замечаний нет."
+
+    def test_condition_2(self):
         select_td_el = self.driver.find_element(By.ID, "td")
 
-        # The second method is below
+        # The second method
         # select_td = Select(select_td_el)
         # select_td.select_by_visible_text("НЕТ")
         select_td_el.click()
@@ -31,9 +46,9 @@ class UiTestMhk01(unittest.TestCase):
         select_opt_os.click()
 
         warning_container = self.driver.find_element(By.ID, "warning_container")
-        print(warning_container.find_element(By.TAG_NAME, "p").text)
-        time.sleep(5)
-        print(warning_container.find_element(By.TAG_NAME, "p").text)
+        # print(warning_container.find_element(By.TAG_NAME, "p").text)
+        # time.sleep(5)
+        # print(warning_container.find_element(By.TAG_NAME, "p").text)
         assert warning_container.find_element(By.TAG_NAME, "p").text == "- Отдельно стоящий механизм (ОС) должен иметь диван."
 
     def tearDown(self):
